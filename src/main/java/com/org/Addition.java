a4e7ae8a82301d95cc9813dcffa524cca1b42793
vishalinagathevan <vishali.nagathevan@gmail.com>
2023-06-06 07:56:21 +0530

    Initial commit

---
+++ src/main/java/com/org/Addition.java
@@ -0,0 +1,14 @@
+package com.org;
+
+public class Addition {
+
+	public int sum(int number1, int number2) {
+		return number1+number2;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Addition addition =new Addition();
+		System.out.print(addition.sum(10, 20 ));
+	}
+}
